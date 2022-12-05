7277003acf3f2a03abd153d243276426674c38c0
hhmadan <hhmadan03@gmail.com>
2022-12-06 00:18:52 +0530

    REFACTORED: Changes done to code

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -3,36 +3,30 @@
 //UC3_Add Part and Full-time Daily Employee Wage
 //UC4_Solve Using Switch Case
 //UC5_Calculate Wage For a Month
-//UC6_Condition For Reaching Limit Of Working Hours (100 Hrs.) OR Days(20 days) In a Month 
-//UC7_Create Class Methods to Compute Employee Wage
+//UC6_Condition For Reaching Limit of Working Hours (100 Hrs.) OR Days(20 days) In a Month 
 
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
 using namespace std;
 
-typedef struct empWage{
-    int wagePerHr;
+        void getMonthlyWage(){
+            int wagePerHr;
     int fullDayHr;
     int halfDayHr;
-    int fullDaySal;
+    int fullDaySal; 
     int halfDaySal;
     int workingDaysInMonth;
-}emp;
 
-class EmployeeWage{
-    public:
-        void getMonthlyWage(){
-            emp e;
-            e.wagePerHr = 20;
-            e.fullDayHr = 8;
-            e.halfDayHr = 4;
-            e.fullDaySal = e.wagePerHr * e.fullDayHr;
-            e.halfDaySal = e.wagePerHr * e.halfDayHr;
+            wagePerHr = 20;
+            fullDayHr = 8;
+            halfDayHr = 4;
+            fullDaySal = wagePerHr * fullDayHr;
+            halfDaySal = wagePerHr * halfDayHr;
             int salPerDay = 0;
             int salPerMonth = 0;
             int workingHrs = 0;
-            int maxWorkingHrs = 0;
+            int maxWorkingHrs = 0; 
             
             srand(time(0));
             for(int day = 1, workingHrs = 0; day <= 20
@@ -47,14 +41,14 @@ class EmployeeWage{
                         cout<<"Salary is $"<<salPerDay<<endl;
                         break;
                     case 1:
-                        salPerDay = e.halfDaySal;
-                        workingHrs = e.halfDayHr;
+                        salPerDay = halfDaySal;
+                        workingHrs = halfDayHr;
                         cout<<"DAY "<<day<<"==> Employee is Present for HALF Day\n";
                         cout<<"Salary is $"<<salPerDay<<endl;
                         break;
                     case 2:
-                        salPerDay = e.fullDaySal;
-                        workingHrs = e.fullDayHr;
+                        salPerDay = fullDaySal;
+                        workingHrs = fullDayHr;
                         cout<<"DAY "<<day<<"==> Employee is Present for FULL Day\n";
                         cout<<"Salary is $"<<salPerDay<<endl;
                         break;  
@@ -66,10 +60,8 @@ class EmployeeWage{
             }
             cout<<"Employee Wage of a Month is: $"<<salPerMonth;    
         }
-};
 
 int main(){
-    EmployeeWage object;
-    object.getMonthlyWage();
+    getMonthlyWage();
     return 0;
 }
